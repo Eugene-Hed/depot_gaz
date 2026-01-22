64796e2a3d7726106a70bc10bdd68c349733de04
Eugene-Hed <simohedric2023@gmail.com>
2026-01-22 10:43:33 +0100

    version 2.0

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Laravel Vercel bootstrap loader
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Create the application instance
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+// Handle the request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+$kernel->terminate($request, $response);
