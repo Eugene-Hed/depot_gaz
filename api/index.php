0775011ef40e029af8ef4197335508058c7eb839
Eugene-Hed <simohedric2023@gmail.com>
2026-01-22 12:12:57 +0100

    fix: correct autoload path for vercel

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php
 
 // Laravel Vercel bootstrap loader
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Create the application instance
-$app = require_once __DIR__ . '/bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // Handle the request
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
